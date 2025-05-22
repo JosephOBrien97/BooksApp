cda6460c375d81226538e7459c8dec6947bb02c0
Joseph OBrien <Joseph.obrien97@hotmail.com>
2025-05-22 17:38:27 +0200

    fix7

--- BooksAngular/src/app/services/quote.service.ts
+++ BooksAngular/src/app/services/quote.service.ts
@@ -9,7 +9,7 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class QuoteService {
-  private apiUrl = `${environment.apiUrl}/quote`;
+  private apiUrl = `${environment.apiUrl}/quotes`;
 
   constructor(
     private http: HttpClient,
